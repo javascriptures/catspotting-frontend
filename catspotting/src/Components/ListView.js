c2fae33f857aa5627d41fd6b6389e109ca128126
jnlsings <jnlsings@gmail.com>
2020-03-05 13:10:53 -0600

    Fetch all posts success

--- catspotting/src/Components/ListView.js
+++ catspotting/src/Components/ListView.js
@@ -1,57 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import error from '../images/error.png'
+import error from '../images/error.png';
 import { Link, Route } from 'react-router-dom';
-import { APIURL } from '../config'
+import { APIURL } from '../config';
 import PostDetail from './PostDetail';
 
 const ListView = () => {
-    //basically the homepage of the app: is accessible upon login or signup
-    //maps out and formats all the posts from the Posts API
-    //fetches from /posts/
+  //basically the homepage of the app: is accessible upon login or signup
+  //maps out and formats all the posts from the Posts API
+  //fetches from /posts/
 
-    const [posts, setPosts] = useState([]);
-    const [error, setError] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        fetch(`${APIURL}/posts`)
-            .then(response => response.json())
-            .then(posts => {
-                setPosts(posts);
-            })
-            .catch(error => {
-                console.log('Error: ', error);
-                setError(true);
-            })
-    }, []);
+  useEffect(() => {
+    fetch(`${APIURL}/posts`)
+      .then(response => response.json())
+      .then(posts => {
+        setPosts(posts);
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+        setError(true);
+      });
+  }, []);
 
-    if(error) {
-        return (
-        <div>
-            <p>Sorry, we're working on it...</p>
-            <img src={error} alt="error kitty"/>>
-        </div>)
-    }
+  if (error) {
+    return (
+      <div>
+        <p>Sorry, we're working on it...</p>
+        <img src={error} alt="error kitty" />
+      </div>
+    );
+  }
 
-    return(
-        <div>
-            {posts.map(post => (
-                <Link to={`posts/${post._id}`}>
-                    <Route 
-                    exact path ={'/posts/:id'}
-                    render={props => {
-                        return <PostDetail match={props.match} />
-                    }}
-                    />
-                    <div>
-                        <img src={post.img_url} alt='kitty post'/>
-                        <h4>{post.username}</h4>
-                        <p>{post.body}</p>
-                        <p>View Comments</p>
-                    </div>  
-                </Link>
-            ))}
-        </div>
-    )
+  return (
+    <div>
+      {posts.map(post => (
+        <Link to={`posts/${post._id}`}>
+          <Route
+            exact
+            path={'/posts/:id'}
+            render={props => {
+              return <PostDetail match={props.match} />;
+            }}
+          />
+          <div>
+            <img src={post.img_url} alt="kitty post" />
+            <h4>{post.author}</h4>
+            <p>{post.body}</p>
+            <p>View Comments</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default ListView;
