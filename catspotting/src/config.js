3e641770a73c41b0ad7edc06515033d03f29d026
javascriptures <jtjavascripture@gmail.com>
2020-03-05 10:46:42 -0800

    ListView done

--- catspotting/src/config.js
+++ catspotting/src/config.js
@@ -1,4 +1,4 @@
 export const APIURL =
   window.location.hostname === 'localhost'
-    ? 'http://localhost:4000'
-    : 'htttp://localhost:4000'; //replace second one with heroku link
+    ? 'http://localhost:8000'
+    : 'htttp://localhost:8000'; //replace second one with heroku link
