faed76527676883c2e6a5f2ad6e2dc22d6ffc856
jnlsings <jnlsings@gmail.com>
2020-03-06 15:09:52 -0600

    Set up Nav.js
    Nav will display differently depending on whether user is logged in or out

--- catspotting/src/App.js
+++ catspotting/src/App.js
@@ -1,47 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {BrowserRouter, withRouter, Route, Switch} from 'react-router-dom';
-import About from './Components/About'
-import CommentForm from './Components/CommentForm'
-import CommentTemplate from './Components/CommentTemplate'
-import Header from './Components/Header'
-import ListView from './Components/ListView'
-import Login from './Components/Login'
-import PostDetail from './Components/PostDetail'
-import PostForm from './Components/PostForm'
-import Signup from './Components/Signup'
-import UserProfile from './Components/UserProfile'
-import Welcome from './Components/Welcome'
+import { BrowserRouter, withRouter, Route, Switch } from 'react-router-dom';
+import About from './Components/About';
+import CommentForm from './Components/CommentForm';
+import CommentTemplate from './Components/CommentTemplate';
+import Header from './Components/Header';
+import ListView from './Components/ListView';
+import Login from './Components/Login';
+import PostDetail from './Components/PostDetail';
+import PostForm from './Components/PostForm';
+import Signup from './Components/Signup';
+import UserProfile from './Components/UserProfile';
+import Welcome from './Components/Welcome';
 
 const App = () => {
-
   const [displayed_form, setDisplay_form] = useState('');
-  const [logged_in, setLogged_in] = useState(localStorage.getItem('token') ? true : false)
-  const [username, setUsername] = useState('')
+  const [logged_in, setLogged_in] = useState(
+    localStorage.getItem('token') ? true : false
+  );
+  const [username, setUsername] = useState('');
 
-  useEffect (() => {
-    if(logged_in) {
+  useEffect(() => {
+    if (logged_in) {
       fetch('change.this', {
         headers: {
           Authorization: `JWT ${localStorage.getItem('token')}`
         }
       })
-      .then(res => res.json())
-      .then(json => {
-        setUsername(json.username)
-      })
+        .then(res => res.json())
+        .then(json => {
+          setUsername(json.username);
+        });
     }
-  })
+  });
 
-
-
-    return (
-      <>
+  return (
+    <>
       <BrowserRouter>
-
-        {
-        window.location.pathname!=='/' | '/login' | '/signup' ? <Header/>:null
-        }
+        {/* only show header if User is NOT on the following paths */}
+        {(window.location.pathname !== '/') | '/login' | '/signup' ? (
+          <Header />
+        ) : null}
         <main>
           <Switch>
             <Route exact path="/" component={Welcome} />
@@ -52,13 +51,17 @@ const App = () => {
             <Route exact path="/commentform" component={CommentForm} />
             <Route exact path="/commenttemplate" component={CommentTemplate} />
             <Route exact path="/posts/:id" component={PostDetail} />
-            <Route exact path="/posts/:id/comments/:id" component={CommentForm} />
+            <Route
+              exact
+              path="/posts/:id/comments/:id"
+              component={CommentForm}
+            />
             <Route exact path="/postform" component={PostForm} />
             <Route exact path="/user/:id/" component={UserProfile} />
           </Switch>
         </main>
       </BrowserRouter>
-      </>
-    );
-  };
-  export default withRouter(App);
+    </>
+  );
+};
+export default withRouter(App);
