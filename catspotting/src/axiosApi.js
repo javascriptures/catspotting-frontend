ee709ad08102c99fd1006c6a38128c947a2094a1
jnlsings <jnlsings@gmail.com>
2020-03-06 17:44:33 -0600

    Add axiosAPI to config headers

---
+++ catspotting/src/axiosApi.js
@@ -0,0 +1,13 @@
+// code borrowed from https://hackernoon.com/110percent-complete-jwt-authentication-with-django-and-react-2020-iejq34ta
+
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000/api/',
+  timeout: 5000,
+  headers: {
+    Authorization: 'JWT ' + localStorage.getItem('access_token'),
+    'Content-Type': 'application/json',
+    accept: 'application/json'
+  }
+});
