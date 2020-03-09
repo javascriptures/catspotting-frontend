8006325aeaf5a261178ff4f8a08f50bcd901259f
javascriptures <jtjavascripture@gmail.com>
2020-03-09 11:17:24 -0700

    pop up description + comments (unfinished, needs formatting)

--- catspotting/src/Components/PostContainer.js
+++ catspotting/src/Components/PostContainer.js
@@ -4,18 +4,25 @@ import CommentTemplate from './CommentTemplate'
 
 
 function PostContainer({post}){
-    const [opacity, setOpacity] = useState('1')
+    const [opacity, setOpacity] = useState('1');
+    const [imgActive, setimgActive] = useState(false);
 
     function showDetails() {
         setOpacity(0.5)
+        setimgActive(true)
     }
     function mouseAway() {
         setOpacity(1)
+        setimgActive(false)
     }
 return(
-    <div>
-            <p>{post.location}</p>
-            <img src={post.img_url} alt="kitty post" onMouseEnter={showDetails} onMouseLeave={mouseAway} style={{opacity: opacity}}/>
+    <div className={`${imgActive ? "show-description" : ""}`} id="postContainer">
+            <h4>{post.location}</h4>
+            <img src={post.img_url} alt="kitty post" onMouseEnter={showDetails} onMouseLeave={mouseAway} style={{opacity: opacity}} className="kittyPics"/>
+            <p className="description">{post.author} <small>{post.body} <br /> 
+                {post.comments.map(comment => 
+                    <p>{comment}</p>
+                )}</small></p>
     </div>
 )
 }
