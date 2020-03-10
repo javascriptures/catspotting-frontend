c5b44fead1dfa80ad91d71df609459ab45978434
javascriptures <jtjavascripture@gmail.com>
2020-03-09 20:18:49 -0700

    About Page

--- catspotting/src/Components/About.js
+++ catspotting/src/Components/About.js
@@ -9,10 +9,21 @@ const About = () => {
     return (
         <>
         <div className="aboutdiv">
-        <h1>About</h1>
-        <p>Catspotting is a social platform made by cat lovers for cat lovers.</p>
+        <h1 className="aboutheader">About</h1>
+        <p>Catspotting is a social platform made by cat lovers for cat lovers. We'll put more of a description in here later.</p>
 
         <h3>Meet the devs</h3>
+        <div className="headshotdiv">
+            <div id="devprofile">
+                <img className="headshots"src={require('../images/JaimieLoweSopranoProfilePic.jpeg')}/>
+                {/* we can put something different below. I'm just bored and it's late */}
+                <p className="devtext">Hi I'm Jaimie and I like cats. I worked really hard on the backend but now I'm stuck in this computer screen. Please help me, no one can hear me in here.</p>
+            </div>
+            <div className="devprofile">
+                <img className="headshots"src={require('../images/Jordan_headshot.JPG')}/>
+                <p className="devtext">Hi I'm Jordan. I like cats and juiceboxes. I curse a lot and no one wants to hire me.</p>
+            </div>
+        </div>
         </div>
         </>
     );
