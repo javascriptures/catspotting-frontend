9bf107fefd83cb860206f5b89a39b7cc7409544e
javascriptures <jtjavascripture@gmail.com>
2020-03-12 17:56:39 -0700

    WOW EDIT WORKS

--- catspotting/src/Components/PostDetail.js
+++ catspotting/src/Components/PostDetail.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
-import {Redirect, useHistory} from 'react-router-dom'
+import {Redirect, useHistory, Link} from 'react-router-dom'
 import { UserContext } from './UserContext';
 import { APIURL } from '../config';
 import CommentTemplate from './CommentTemplate';
 import PostEdit from './PostEdit'
+import TextField from '@material-ui/core/TextField';
 
 function PostDetail({ match }) {
   //shows post on a single page
@@ -14,6 +15,7 @@ function PostDetail({ match }) {
 
   const [post, setPost] = useState(null);
   const [error, setError] = useState(null);
+  const [edit, setEdit] = useState(false);
   const history = useHistory();
   const useMountEffect = post => useEffect(post, []);
 
@@ -32,10 +34,29 @@ function PostDetail({ match }) {
 
   useMountEffect(fetchData);
 
-  function handleEdit() {
-    return(
-      <Redirect to={{PostEdit}}/>
-    )
+  const handleChange = (event) => {
+    setPost({ ...post, [event.target.name]: event.target.value });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //POST request to API
+    fetch(`${APIURL}/posts/${match.params.id}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+        Authorization: `Bearer ${user.access}`
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(setPost)
+      .catch(error => {
+        console.log('Logged error: ', error);
+        setError(true);
+        console.log(user);
+      });
+      setEdit(false);
   }
 
   const handleDelete = () => {
@@ -56,6 +77,7 @@ function PostDetail({ match }) {
       history.goBack();
   };
 
+
   if (post) {
     return (
       <>
@@ -69,10 +91,62 @@ function PostDetail({ match }) {
           <p className="body">
           {post.body}
           </p>
-
-          <img src={require('../images/edit.png')} className="icons"/>
+          <img src={require('../images/edit.png')} className="icons" onClick={() => setEdit(true)}/>
           <img src={require('../images/delete.png')}className="icons" onClick={handleDelete}/>
         </div>
+        {edit &&
+        <div>
+          <form onSubmit={handleSubmit}>
+          <p>
+            <TextField
+              className="textfields"
+              id="outlined-multiline-static"
+              label="Image url"
+              multiline
+              rows="4"
+              variant="outlined"
+              value={post.img_url}
+              name="img_url"
+              onChange={handleChange}
+            />
+            <br />
+          </p>
+          <p>
+            <TextField
+              className="textfields"
+              id="outlined-multiline-static"
+              label="Description"
+              multiline
+              rows="4"
+              variant="outlined"
+              value={post.body}
+              name="body"
+              onChange={handleChange}
+            />
+            <br />
+          </p>
+          <p>
+            <TextField
+              className="textfields"
+              id="outlined-basic"
+              label="Location"
+              variant="outlined"
+              value={post.location}
+              name="location"
+              onChange={handleChange}
+            />
+          </p>
+          <div className="postbutton">
+        <button
+          className="welcomebuttons"
+          id="postbutton"
+          onClick={handleSubmit}
+        >
+          Post!
+        </button>
+      </div>
+        </form>
+        </div>}
         <ul>
           {post.comments && post.comments.map(comment => (
             <div key={comment} className="descriptioncontainer">
